f14a119fee2c1f62e4bda62247a2a7128bf3197f
jclinsim <96226187+jclinsim@users.noreply.github.com>
2022-03-02 12:59:38 +1100

    Created search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary</header>
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <main>
+          {" "}
+          <Dictionary />{" "}
+        </main>
+        <footer className="text-center">
+          <small> Coded by Johanna Lingard </small>
+        </footer>
+      </div>
     </div>
   );
 }
